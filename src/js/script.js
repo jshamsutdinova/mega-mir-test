9a1540e1712d09be8a909a66ae5f56303ed94391
Julia Shamsutdinova <shjn0297@gmail.com>
2022-04-21 18:57:56 +0800

    Added click event to buttons

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,31 @@
-@@include('alert.js')
+// Fix the Header
+window.onscroll = function() {fixHeader()};
+
+var header = document.getElementById("header");
+
+var sticky = header.offsetTop;
+
+function fixHeader() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
+
+// Buttons
+function buyClick() {
+    console.log("Товар добавить в корзину");
+}
+
+var discountMenuBtn = document.getElementById("discountMenu");
+
+discountMenuBtn.onclick = function() {
+    console.log('Open discounts');
+}
+
+var btnMoreDetails = document.querySelector('.btn__more');
+
+btnMoreDetails.onclick = function() {
+    console.log("Get more details")
+}
